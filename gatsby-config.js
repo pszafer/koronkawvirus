ac42273f99e266aa2a247710fd4e1913085229c1
Paweł Szafer <pszafer@gmail.com>
2020-04-05 19:21:00 +0200

    minimal-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#f1e9e9`,
         theme_color: `#f1e9e9`,
-        display: `standalone`,
+        display: `minimal-ui`,
         icon: `src/images/koronka_ico.png`, // This path is relative to the root of the site.
       },
     },
